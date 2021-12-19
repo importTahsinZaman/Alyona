790b4f12d5aa7f74d87c5edcc3c417e29c4aee09
importTahsinZaman <86907892+importTahsinZaman@users.noreply.github.com>
2021-12-19 00:59:33 -0500

    Minor Updates

--- Door.gd
+++ Door.gd
@@ -7,15 +7,15 @@ export (int) var left_clamp
 export (int) var right_clamp
 
 func _ready():
-	$Label.text = "(E) Enter"
+	if building != null: $Label.text = "(E) Enter"
+	elif building == null: $Label.text = "(E) Exit"
 	$Label.visible = false
 
 func _physics_process(delta):
 	if overlaps_body(body) and Input.is_action_just_pressed("interact"):
-		get_parent().change_house(building, left_clamp, right_clamp)
+		Global.change_house(building, left_clamp, right_clamp)
 		if building != null:
 			Global.last_player_location = self.position
-
 
 func _on_Area2D_body_entered(body): $Label.visible = true
 func _on_Area2D_body_exited(body): $Label.visible = false
--- Global.gd
+++ Global.gd
@@ -3,3 +3,13 @@ extends Node
 var camera_clamps = Vector2(-1185, 2255)
 var last_player_location = Vector2(162,130)
 var return_to_town = true
+
+
+func change_house(building : PackedScene, left_clamp, right_clamp):
+	return_to_town = !return_to_town
+	if building != null: 
+		camera_clamps = Vector2(left_clamp, right_clamp)
+		get_tree().change_scene(building.resource_path)
+	else:
+		camera_clamps = Vector2(-1185, 2255)
+		get_tree().change_scene("res://.mono/World.tscn")
