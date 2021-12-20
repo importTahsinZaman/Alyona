3cc35f6ec86f7404dec4610aeb2e5ef25e1c87e7
importTahsinZaman <86907892+importTahsinZaman@users.noreply.github.com>
2021-12-19 22:08:44 -0500

    Changed Player to Alyona

--- Player.gd
+++ Player.gd
@@ -1,10 +1,10 @@
 extends KinematicBody2D
 
-const MAXSPEED = 90
+const MAXSPEED = 80
 const GRAVITY = 20
 const MAXFALLSPEED = 200
 const UP = Vector2(0,-1)
-const ACCEL = 14
+const ACCEL = 10
 
 var motion = Vector2()
 
