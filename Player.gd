c66a384abbd387ab2152a2ea0e09653b156882b6
importTahsinZaman <86907892+importTahsinZaman@users.noreply.github.com>
2021-12-24 16:04:31 -0500

    Added attack animation

--- Player.gd
+++ Player.gd
@@ -1,5 +1,13 @@
 extends KinematicBody2D
 
+enum{
+	MOVE,
+	IDLE,
+	ATTACK
+}
+
+var state = IDLE
+
 const MAXSPEED = 60
 const GRAVITY = 20
 const MAXFALLSPEED = 200
@@ -8,18 +16,30 @@ const ACCEL = 8
 
 var motion = Vector2()
 
+var attacking = false
+var attack_cooldown = 0
+
 func _ready():
 	if Global.return_to_town: self.position = Global.last_player_location
 	Global.player_body = self
 	
+func set_attacking(): attacking = false
+	
 func _physics_process(delta):
+	attack_cooldown += 1 * delta
 	motion.y += GRAVITY
 	if motion.y > MAXFALLSPEED:
 		motion.y = MAXFALLSPEED
 	
 	motion.x = clamp(motion.x, -MAXSPEED, MAXSPEED)
 	
-	if Input.is_action_pressed("ui_left") and self.position.x > Global.camera_clamps.x + 15:
+	if attacking: 
+		motion.x = lerp(motion.x, 0, 0.2)
+		$AnimationPlayer.play("Attack")
+	elif Input.is_action_pressed("interact"):
+		motion.x = lerp(motion.x, 0, 0.2)
+		attacking = true
+	elif Input.is_action_pressed("ui_left") and self.position.x > Global.camera_clamps.x + 15:
 		$Sprite.flip_h = true
 		$AnimationPlayer.play("Walk")
 		motion.x -= ACCEL
